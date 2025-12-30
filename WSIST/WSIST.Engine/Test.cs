11ed0b76902b74dcef6763700afd6a2d547a14ff
Tim <tim.hug@digitecgalaxus.ch>
2025-12-30 12:46:17 +0100

    Enums are now Dispaly Cleanly to the user

--- WSIST/WSIST.Engine/Test.cs
+++ WSIST/WSIST.Engine/Test.cs
@@ -32,11 +32,77 @@ public class Test
 
     public enum PersonalUnderstanding
     {
-        VeryLow,
-        Low,
-        Medium,
-        Average,
-        High,
-        VeryHigh,
+        VeryLow = 0,
+        Low = 1,
+        Medium = 2,
+        Average = 3,
+        High = 4,
+        VeryHigh = 5,
+    }
+
+    public static string VolumeHelper(TestVolume volume)
+    {
+        switch (volume)
+        {
+            case TestVolume.VeryLow:
+            {
+                return "Very Low";
+            }
+            case TestVolume.Low:
+            {
+                return "Low";
+            }
+            case TestVolume.Medium:
+            {
+                return "Medium";
+            }
+            case TestVolume.Average:
+            {
+                return "Average";
+            }
+            case TestVolume.High:
+            {
+                return "High";
+            }
+            case TestVolume.VeryHigh:
+            {
+                return "Very High";
+            }
+        }
+
+        return "Please Choose a Setting";
+    }
+    
+    public static string UnderstandingHelper(PersonalUnderstanding volume)
+    {
+        switch (volume)
+        {
+            case PersonalUnderstanding.VeryLow:
+            {
+                return "Very Low";
+            }
+            case PersonalUnderstanding.Low:
+            {
+                return "Low";
+            }
+            case PersonalUnderstanding.Medium:
+            {
+                return "Medium";
+            }
+            case PersonalUnderstanding.Average:
+            {
+                return "Average";
+            }
+            case PersonalUnderstanding.High:
+            {
+                return "High";
+            }
+            case PersonalUnderstanding.VeryHigh:
+            {
+                return "Very High";
+            }
+        }
+
+        return "Please Choose a Setting";
     }
 }
