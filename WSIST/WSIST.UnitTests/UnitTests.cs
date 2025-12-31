dea341e1254f31e99d8d67f7c123d8030af5651e
Tim <tim.hug@digitecgalaxus.ch>
2025-12-31 14:22:54 +0100

    Some More ocmmits

---
+++ WSIST/WSIST.UnitTests/UnitTests.cs
@@ -0,0 +1,26 @@
+﻿using WSIST.Engine;
+
+namespace WSIST.UnitTests;
+
+[TestClass]
+public sealed class UnitTests
+{
+    [TestMethod]
+    [Obsolete("Obsolete")]
+    public void TestIfNewTestGetsMade()
+    {
+        //arrange
+        var manager = new TestManagement();
+        //act
+        manager.NewTestMaker(
+            "Math Tets",
+            Test.Subjects.Math,
+            new DateOnly(2026, 02,04),
+            Test.TestVolume.VeryHigh,
+            Test.PersonalUnderstanding.VeryLow,
+            4.5
+        );
+        //assert
+        Assert.Equals("Math Test", manager.Tests.First().Title);
+    }
+}
